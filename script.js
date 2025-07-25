5b891522fa34c66b1c015c962d09c48cd562e53d
Dhanasri Emani <emanidanasreereddye111@gmail.com>
2025-07-25 18:43:18 +0530

    Login page

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getAuth,
-  signInWithEmailAndPassword,
   signInWithPopup,
   GoogleAuthProvider,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
@@ -13,29 +12,32 @@ const firebaseConfig = {
   storageBucket: "virtual-study-room-95bb1.appspot.com",
   messagingSenderId: "995781311316",
   appId: "1:995781311316:web:145128a95373a7d2f1a6c1",
-  measurementId: "G-D9DN8X4HH2"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-window.login = function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+// Normal Login (just for redirection demo)
+window.normalLogin = function () {
+  const username = document.getElementById("username").value;
+  const age = document.getElementById("age").value;
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "homepage.html";
-    })
-    .catch((error) => {
-      alert("Login error: " + error.message);
-    });
+  if (username && age) {
+    // You can store these in localStorage or Firestore
+    localStorage.setItem("username", username);
+    localStorage.setItem("age", age);
+    window.location.href = "homepage.html";
+  } else {
+    alert("Please fill both fields");
+  }
 };
 
-window.handleCredentialResponse = function (response) {
+// Google Login
+window.googleLogin = function () {
   signInWithPopup(auth, provider)
-    .then(() => {
+    .then((result) => {
+      localStorage.setItem("username", result.user.displayName);
       window.location.href = "homepage.html";
     })
     .catch((error) => {
